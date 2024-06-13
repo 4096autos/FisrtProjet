a4b426a047f8fb2d77f9be14dfc9a169d09edfbf
4096autos <4096autos@gmail.com>
2024-06-13 16:15:07 +0000

    First Class | Class - Constructeur - OBJET/instance - this

---
+++ App.java
@@ -0,0 +1,10 @@
+public class App
+{
+    public static void main(String []args)
+    {
+        //System.out.println("Hello");
+        Cat miaou = new Cat();
+        Cat miaou2 = new Cat();
+
+    }
+}
